862db20e63e881c5fa674456a9206e1286e15ad4
laura.chevillet <laura.chevillet@ccicampus.fr>
2026-04-02 08:52:32 +0200

    syncro

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,8 @@
 import './stimulus_bootstrap.js';
 
-//import de Bootstrap
+// Import de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
 
-
-//import du css de l'application
+// Import du CSS de l'application
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(Request $request): Response
+    {
+        //dd($request->query->get('last', 10)); -> var_dump(..); die;
+
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
