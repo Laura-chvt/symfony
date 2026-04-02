2a02460f7dcb34ebe260423f61df00dd02735b3c
laura.chevillet <laura.chevillet@ccicampus.fr>
2026-04-02 11:29:01 +0200

    update

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -11,9 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/pokemon', name: 'app_pokemon_')]
 final class PokemonController extends AbstractController
 {
-    #[Route('/pokemon', name: 'app_pokemon')]
+    #[Route('/', name: 'index')]
     public function index(PokemonRepository $pokemonRepository): Response
     {
         $arrPokemon = $pokemonRepository->findAll();
@@ -23,74 +24,73 @@ final class PokemonController extends AbstractController
         ]);
     }
 
-    #[Route('/pokemon/create', name: 'app_pokemon_create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
-        /*
-        if($request->isMethod('POST')) {
-
-            $onError = false; //< Flag qui vérifie si tout est OK ou pas
-
-            $strPokemonName     = $request->request->get('name');
-            $intPokemonNumber   = $request->request->get('number');
-
-            if($intPokemonNumber <= 0) {
-
-                $onError = true;
-
-                // Je stocke un message "flash" dans la session
-                // Attention, flash nécessite une redirection propre '$this->redirectTo...'
-                // $this->addFlash('danger', "Le numéro dans le Pokédex doit être supérieur à zéro");
-            }
-
-            if(!$onError) {
-
-                $objPokemon = new Pokemon();
-
-                $objPokemon->setName($strPokemonName)
-                        ->setNumber($intPokemonNumber);
-
-                $entityManager->persist($objPokemon);
-
-                $entityManager->flush();
-
-                $this->addFlash('success', "Le Pokémon a été créé avec succès !");
-
-                // On redirige vers la page de détails du pokémon qui vient d'être enregistré en base
-                return $this->redirectToRoute('app_pokemon_show', [
-                    'id' => $objPokemon->getId()
-                ]);
-            }
-        }
-        */
-
         $objNewPokemon = new Pokemon();
 
         $createForm = $this->createForm(PokemonCreateFormType::class, $objNewPokemon);
+
+        // J'envoi les données de la requête au formulaire
+        $createForm->handleRequest($request);
+
+        // Vérifie si le formulaire est soumiii et que les données sont valides
+        if($createForm->isSubmitted() && $createForm->isValid()) {
+
+            $entityManager->persist($objNewPokemon);
+            $entityManager->flush();
+
+            // Affiche un message de succès
+            $this->addFlash('success', "Le pokémon a bien été créé en base");
+
+            // Redirige vers la page de détails du pokémon créé
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $objNewPokemon->getId()
+            ]);
+        }
         
-        return $this->render('pokemon/create.html.twig', [
+        return $this->render('pokemon/form.html.twig', [
             'createForm'    => $createForm
         ]);
     }
 
-    #[Route('/pokemon/{id<\d+>}', name: 'app_pokemon_show')]
+    #[Route('/{id<\d+>}', name: 'show')]
     public function show(Pokemon $pokemon): Response
     {
-        /*
-        $objPokemon = $pokemonRepository->find($id);
-
-        if(!$objPokemon) {
-            throw $this->createNotFoundException(
-                "Le Pokémon n'existe pas"
-            );
-        }
-        */
-
         return $this->render('pokemon/show.html.twig', [
             /*'pokemon_id'    => $id,
             'pokemon'       => $objPokemon
             */
             'pokemon'       => $pokemon
+        ]);
+    }
+
+    #[Route('/{id<\d+>}/update', name: 'update')] //< URL : /pokemon/1/update
+    public function update(Pokemon $pokemon, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // On construit le formulaire à partir des données de l'entité récupérée
+        // depuis l'ID présent dans l'URL
+        $updateForm = $this->createForm(PokemonCreateFormType::class, $pokemon);
+
+        $updateForm->handleRequest($request);
+
+        if($updateForm->isSubmitted() && $updateForm->isValid()) {
+
+            // L'entité provenant déjà de la base, Doctrine la connait
+            // => pas besoin de persist
+
+            $entityManager->flush();
+
+            $this->addFlash('success', "Le pokémon a bien été modifié en base");
+
+            // Redirige vers la page de détails du pokémon modifié
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $pokemon->getId()
+            ]);
+        }
+
+        return $this->render('pokemon/form.html.twig', [
+            'createForm'    => $updateForm
         ]);
     }
 }
